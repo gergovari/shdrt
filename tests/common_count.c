c9b1d16b3078bad493cce74d6346385a4c46bbd7
Gergo Vari <work@gergovari.com>
2025-12-24 18:35:05 +0100

    misc: tests

---
+++ tests/common_count.c
@@ -0,0 +1,26 @@
+#include <unity.h>
+
+#include "../src/common.h"
+
+#define EXPECTED_COUNT 4
+#define LIST(X) \
+        X(ONE) \
+        X(TWO) \
+        X(THREE) \
+        X(FOUR) 
+#define LIST_COUNT (0 LIST(COUNT_ITEMS))
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_common_count(void) {
+	TEST_ASSERT_EQUAL_INT(EXPECTED_COUNT, LIST_COUNT);
+}
+
+int main(void) {
+	UNITY_BEGIN();
+
+	RUN_TEST(test_common_count);
+
+	return UNITY_END();
+}
