c887a5f71922af4a3fa9af117de0e16542f98dbe
Gergo Vari <work@gergovari.com>
2025-12-24 18:59:49 +0100

    fix: enable stop callback

--- src/service/map.c
+++ src/service/map.c
@@ -1,11 +1,11 @@
 #include "map.h"
 
-/*shdrt_ServiceContext* shdrt_ServiceMap_add(shdrt_ServiceMap* map, shdrt_Service s, void* user, shdrt_ServiceStopCallback stop) {
+shdrt_ServiceContext* shdrt_ServiceMap_add(shdrt_ServiceMap* map, shdrt_Service s, void* user, shdrt_ServiceStopCallback stop) {
 	shdrt_ServiceContext* ctx = c_new(shdrt_ServiceContext, {.continuation_mode = 0, .user = user, .stop = stop});
 	shdrt_ServiceMap_result res = shdrt_ServiceMap_insert(map, s, ctx);
 
 	return res.inserted ? res.ref->second : NULL;
-}*/
+}
 
 bool shdrt_ServiceMap_delete(shdrt_ServiceMap* map, shdrt_Service s) {
 	const shdrt_ServiceMap_value* val = shdrt_ServiceMap_get(map, s);
