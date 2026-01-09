62e38660e2480152b5457c5fbf8668a1be9384c1
Gergo Vari <work@gergovari.com>
2026-01-09 18:51:34 +0100

    refactor: change Service interface

--- src/service/service.h
+++ src/service/service.h
@@ -3,6 +3,7 @@
 #include <stc/cstr.h>
 
 #include "../component_id.h"
+#include "../intent.h"
 
 #include "continuation_mode.h"
 #include "context.h"
@@ -10,14 +11,12 @@
 #include "remote_interface.h"
 #include "start_flags.h"
 
-typedef struct shdrt_Intent shdrt_Intent;
-
 typedef struct {
 	shdrt_ComponentIdentifier id;
 	shdrt_ServiceContinuationMode (*on_start_command)(shdrt_ServiceContext*, shdrt_Intent, shdrt_ServiceStartFlags, shdrt_ServiceStartId);
 
 	bool (*on_create)(shdrt_ServiceContext*);
-	bool (*on_destroy)(shdrt_ServiceContext*);
+	void (*on_destroy)(shdrt_ServiceContext*);
 
 	shdrt_ServiceRemoteInterface* (*on_bind)(shdrt_Intent);
 } shdrt_Service;
