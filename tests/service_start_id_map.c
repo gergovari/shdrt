fd09bf8b8667810ba42113ddae3b4877ac95a6ef
Gergo Vari <work@gergovari.com>
2026-01-09 16:34:44 +0100

    test: ServiceStartIdMap test for multiples on ops

--- tests/service_start_id_map.c
+++ tests/service_start_id_map.c
@@ -50,7 +50,25 @@ void test_stop(void) {
 	shdrt_ServiceStartIdMap_drop(&map);	
 }
 
-// TODO: test for multiple stop
+void test_stop_multiple(void) {
+	shdrt_Service s = { .id = shdrt_ComponentIdentifier_make("foo", "bar") };
+	shdrt_Service s2 = { .id = shdrt_ComponentIdentifier_make("dead", "beef") };
+
+	shdrt_ServiceStartIdMap map = {0};
+
+	shdrt_ServiceStartId id;
+	shdrt_ServiceStartId id2;
+
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s, &id));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s2, &id2));
+
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_stop(&map, id));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_stop(&map, id2));
+
+	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_stop(&map, id));
+
+	shdrt_ServiceStartIdMap_drop(&map);
+}
 
 void test_forget(void) {
 	shdrt_Service s = { .id = shdrt_ComponentIdentifier_make("foo", "bar") };
@@ -67,7 +85,26 @@ void test_forget(void) {
 	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_contains(&map, id));
 }
 
-// TODO: test for multiple forget
+void test_forget_multiple(void) {
+	shdrt_Service s = { .id = shdrt_ComponentIdentifier_make("foo", "bar") };
+	shdrt_Service s2 = { .id = shdrt_ComponentIdentifier_make("dead", "beef") };
+
+	shdrt_ServiceStartIdMap map = {0};
+
+	shdrt_ServiceStartId id;
+	shdrt_ServiceStartId id2;
+
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s, &id));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s2, &id2));
+	
+	shdrt_ServiceStartIdMap_forget(&map, s);
+	shdrt_ServiceStartIdMap_forget(&map, s2);
+
+	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_contains(&map, id));
+	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_contains(&map, id2));
+
+	shdrt_ServiceStartIdMap_drop(&map);
+}
 
 void test_get_service(void) {
 	shdrt_Service s = { .id = shdrt_ComponentIdentifier_make("foo", "bar") };
@@ -81,7 +118,7 @@ void test_get_service(void) {
 	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s, &id));
 	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_contains(&map, id));
 	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_get_service(&map, id, &s2));
-	TEST_ASSERT_TRUE(shdrt_Service_cmp(&s, &s2) == 0);
+	TEST_ASSERT_TRUE(shdrt_Service_equals(&s, &s2));
 
 	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_stop(&map, id));
 	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_contains(&map, id));
@@ -93,7 +130,33 @@ void test_get_service(void) {
 	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_get_service(&map, id, &s2));
 }
 
-// TODO: test for multiple get_service
+void test_get_service_multiple(void) {
+	shdrt_Service s = { .id = shdrt_ComponentIdentifier_make("foo", "bar") };
+	shdrt_Service s2 = { .id = shdrt_ComponentIdentifier_make("dead", "beef") };
+
+	shdrt_Service sout = { 0 };
+	shdrt_Service s2out = { 0 };
+
+	shdrt_ServiceStartIdMap map = {0};
+
+	shdrt_ServiceStartId id;
+	shdrt_ServiceStartId id2;
+	
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s, &id));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_start(&map, s2, &id2));
+
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_get_service(&map, id, &sout));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_get_service(&map, id2, &s2out));
+
+	TEST_ASSERT_TRUE(shdrt_Service_equals(&s, &sout));
+	TEST_ASSERT_TRUE(shdrt_Service_equals(&s2, &s2out));
+
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_stop(&map, id));
+	TEST_ASSERT_TRUE(shdrt_ServiceStartIdMap_stop(&map, id2));
+
+	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_get_service(&map, id, &sout));
+	TEST_ASSERT_FALSE(shdrt_ServiceStartIdMap_get_service(&map, id, &s2out));
+}
 
 // TODO: test for start_id stop behaviour
 
@@ -104,8 +167,13 @@ int main(void) {
 	RUN_TEST(test_start_multiple);
 
 	RUN_TEST(test_stop);
+	RUN_TEST(test_stop_multiple);
+
 	RUN_TEST(test_forget);
+	RUN_TEST(test_forget_multiple);
+
 	RUN_TEST(test_get_service);
+	RUN_TEST(test_get_service_multiple);
 
 	return UNITY_END();
 }
