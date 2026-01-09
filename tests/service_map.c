8893ae2e6f9bc1de9e368e9080deebb5f6bc896c
Gergo Vari <work@gergovari.com>
2026-01-09 10:39:39 +0100

    test: ServiceMap test multiple add/delete

--- tests/service_map.c
+++ tests/service_map.c
@@ -51,7 +51,39 @@ void test_add(void) {
 	shdrt_ServiceMap_drop(&map);
 }
 
-// TODO: test for multiple add
+void test_add_multiple(void) {
+	shdrt_ServiceMap map = {0};
+	shdrt_Service calc = { .id = { .package = cstr_from("shd"), .name = cstr_from("calculator") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service comm = { .id = { .package = cstr_from("shd"), .name = cstr_from("communication") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service sett = { .id = { .package = cstr_from("shd"), .name = cstr_from("settings") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service thirdp = { .id = { .package = cstr_from("acme"), .name = cstr_from("banking") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, calc, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, comm, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, sett, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, thirdp, NULL, NULL));
+
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_get(&map, calc));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_get(&map, comm));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_get(&map, sett));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_get(&map, thirdp));
+}
 
 void test_delete(void) {
 	shdrt_ServiceMap map = c_make(shdrt_ServiceMap, {*global_s});
@@ -75,13 +107,55 @@ void test_delete(void) {
 	shdrt_ServiceMap_drop(&map);
 }
 
-// TODO: test for multiple delete
+void test_delete_multiple(void) {
+	shdrt_ServiceMap map = {0};
+	shdrt_Service calc = { .id = { .package = cstr_from("shd"), .name = cstr_from("calculator") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service comm = { .id = { .package = cstr_from("shd"), .name = cstr_from("communication") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service sett = { .id = { .package = cstr_from("shd"), .name = cstr_from("settings") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+	shdrt_Service thirdp = { .id = { .package = cstr_from("acme"), .name = cstr_from("banking") },
+		.on_start_command = NULL,
+		.on_create = NULL,
+		.on_destroy = NULL,
+		.on_bind = NULL };
+
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, calc, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, comm, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, sett, NULL, NULL));
+	TEST_ASSERT_NOT_NULL(shdrt_ServiceMap_add(&map, thirdp, NULL, NULL));
+	
+	TEST_ASSERT_TRUE(shdrt_ServiceMap_delete(&map, calc));
+	TEST_ASSERT_TRUE(shdrt_ServiceMap_delete(&map, comm));
+	TEST_ASSERT_TRUE(shdrt_ServiceMap_delete(&map, sett));
+	TEST_ASSERT_TRUE(shdrt_ServiceMap_delete(&map, thirdp));
+	
+	TEST_ASSERT_FALSE(shdrt_ServiceMap_delete(&map, calc));
+
+	TEST_ASSERT_NULL(shdrt_ServiceMap_get(&map, calc));
+	TEST_ASSERT_NULL(shdrt_ServiceMap_get(&map, comm));
+	TEST_ASSERT_NULL(shdrt_ServiceMap_get(&map, sett));
+	TEST_ASSERT_NULL(shdrt_ServiceMap_get(&map, thirdp));
+}
 
 int main(void) {
 	UNITY_BEGIN();
 
 	RUN_TEST(test_add);
+	RUN_TEST(test_add_multiple);
+
 	RUN_TEST(test_delete);
+	RUN_TEST(test_delete_multiple);
 
 	return UNITY_END();
 }
