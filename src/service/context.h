b13ad8dfc13375bd1a7155025018fc43240751de
Gergo Vari <work@gergovari.com>
2026-01-10 10:53:10 +0100

    feat: add ServiceManager to ServiceContext

--- src/service/context.h
+++ src/service/context.h
@@ -4,9 +4,13 @@
 #include "stop_callback.h"
 #include "start_id.h"
 
+typedef struct shdrt_ServiceManager shdrt_ServiceManager;
+
 typedef struct {
 	void* user;
 	shdrt_ServiceContinuationMode continuation_mode;
 	
 	shdrt_ServiceStopCallback stop;
+
+	shdrt_ServiceManager* man;
 } shdrt_ServiceContext;
