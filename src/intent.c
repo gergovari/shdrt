4fcf4f0013968a81c7a00e6b678fd2e5ffbe38cb
Gergo Vari <work@gergovari.com>
2026-01-11 16:27:47 +0100

    feat: intents implemented

---
+++ src/intent.c
@@ -0,0 +1,24 @@
+#include "intent.h"
+
+#include <stc/cstr.h>
+
+#include "component_id.h"
+
+int shdrt_Intent_cmp(const shdrt_Intent* a, const shdrt_Intent* b) {
+	bool id = a->explicitId && b->explicitId ? shdrt_ComponentIdentifier_equals(&a->id, &b->id) : a->explicitId == b->explicitId;
+	bool action = a->explicitAction && b->explicitAction ? cstr_cmp(&a->action, &b->action) == 0 : a->explicitAction == b->explicitAction;
+	bool data = a->explicitData && b->explicitData ? cstr_cmp(&a->data, &b->data) == 0 : a->explicitData == b->explicitData;
+	bool type = a->explicitType && b->explicitType ? cstr_cmp(&a->type, &b->type) == 0 : a->explicitType == b->explicitType;
+	bool category = a->category == b->category;
+
+	if (id && action && data && type && category) return 0;
+
+	if (a->explicitId == b->explicitId && a->explicitId) return shdrt_ComponentIdentifier_cmp(&a->id, &b->id);
+	if (a->explicitAction == b->explicitAction && a->explicitAction) return cstr_cmp(&a->action, &b->action);
+	
+	return a->explicitId ? 1 : -1;
+}
+
+bool shdrt_Intent_equals(const shdrt_Intent* a, const shdrt_Intent* b) {
+	return shdrt_Intent_cmp(a, b) == 0;
+}
--- src/intent.h
+++ src/intent.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#include <stdbool.h>
+
+#include <stc/cstr.h>
+
+#include "component_id.h"
+#include "category_flags.h"
+#include "intent_flags.h"
+
 typedef struct {
-	int dummy; // TODO
+	shdrt_ComponentIdentifier id;
+	bool explicitId;
+
+	cstr action;
+	bool explicitAction;
+
+	cstr data;
+	bool explicitData;
+
+	cstr type;
+	bool explicitType;
+
+	shdrt_CategoryFlags category;
+
+	void* extra;
+	shdrt_IntentFlags flags;
 } shdrt_Intent;
+
+int shdrt_Intent_cmp(const shdrt_Intent* a, const shdrt_Intent* b);
+bool shdrt_Intent_equals(const shdrt_Intent* a, const shdrt_Intent* b);
